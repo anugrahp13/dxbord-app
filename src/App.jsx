744a23fc3f929f14353685da752f63e19ca2e630
anugrahp13 <anugrahprastyo13@gmail.com>
2024-08-02 21:36:14 +0700

    feat [*]: create all

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Header from "./components/Elements/Header";
+import Sidebar from "./components/Elements/Sidebar";
+import Main from "./pages/Main";
+import Content from "./pages/Content";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className={`${darkMode && "dark"}`}>
+      <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} toggleSidebar={toggleSidebar} />
+      <Sidebar isSidebarOpen={isSidebarOpen}/>
 
-export default App
+      <Main>
+        <Content>Main Content</Content>
+      </Main>
+    </div>
+  );
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        'primary': '#14b8a6',
+        'secondary': '#64748b',
+        'dark': '#0f172a',
+      },
+      fontFamily: {
+        quicksand : ['Quicksand', 'sans-serif']
+      },
+    },
   },
   plugins: [],
+  darkMode: 'class',
 }
 
